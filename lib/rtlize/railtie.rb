4f7d0bd138bb111d1dad3035b060769e19eb49a7
kinopyo <fantasyday@gmail.com>
2016-08-05 13:26:34 +0900

    Update processor register syntax to support Sprockets 2, 3 and 4
    It also has the effect of silencing the deprecation message.
    See more:
    https://github.com/rails/sprockets/blob/master/guides/extending_sprockets.md#registering-all-versions-of-sprockets-in-processors

--- lib/rtlize/railtie.rb
+++ lib/rtlize/railtie.rb
@@ -8,7 +8,18 @@ module Rtlize
     config.rtlize.rtl_locales  = Rtlize.rtl_locales
 
     initializer "rtlize.railtie", :after => "sprockets.environment" do |app|
-      app.assets.register_postprocessor 'text/css', Rtlize::RtlProcessor
+      # Support Sprockets 4
+      if app.assets.respond_to?(:register_transformer)
+        app.assets.register_mime_type 'text/css', extensions: ['.css'], charset: :css
+        app.assets.register_postprocessor 'text/css', 'text/css', Rtlize::RtlProcessor
+      end
+
+      # Support Sprockets 2, 3
+      if app.assets.respond_to?(:register_engine)
+        args = ['.css', Rtlize::RtlProcessor]
+        args << { mime_type: 'text/css', silence_deprecation: true } if Sprockets::VERSION.start_with?("3")
+        app.assets.register_engine(*args)
+      end
 
       Rtlize.rtl_selector = config.rtlize.rtl_selector
       Rtlize.rtl_locales  = config.rtlize.rtl_locales
